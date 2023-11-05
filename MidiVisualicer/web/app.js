d8ce374ea5543be7c0a1e1d2c891ccc97cb49021
daniou <danioliveras7@gmail.com>
2023-11-05 22:29:43 +0100

    splitted web in correspondent files

--- MidiVisualicer/web/app.js
+++ MidiVisualicer/web/app.js
@@ -1,49 +1,33 @@
-let player;
-const midiFileInput = document.getElementById('midiFile');
-const playButton = document.getElementById('playButton');
-const visualization = document.getElementById('visualization');
-const audioContext = new Tone.Context();
 
-// Function to load and play MIDI file
-function playMIDI(file) {
-    if (!player) {
-        // Initialize the player only once
-        player = new Tone.Player().toDestination();
-    }
+const midiFileInput = document.getElementById('midiFileInput');
+const staffContent = document.querySelector('.staff-content');
 
-    const reader = new FileReader();
-    reader.onload = (e) => {
-        const dataUri = e.target.result;
-        player.load(dataUri, () => {
-            player.start();
-        });
-    };
-    reader.readAsArrayBuffer(file);
-}
+midiFileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
 
-// Event listener for the play button
-playButton.addEventListener('click', () => {
-    if (player && player.state === 'started') {
-        player.stop();
-        playButton.innerText = 'Play';
-    } else {
-        if (audioContext.state !== 'running') {
-            audioContext.resume().then(() => {
-                // Start audio after user gesture
-                playMIDI(midiFileInput.files[0]);
-                playButton.innerText = 'Stop';
-            });
-        } else {
-            playMIDI(midiFileInput.files[0]);
-            playButton.innerText = 'Stop';
-        }
-    }
+  reader.onload = (readerEvent) => {
+    const midiData = readerEvent.target.result;
+    const midiPlayer = document.querySelector('midi-player');
+    midiPlayer.setAttribute('src', URL.createObjectURL(file));
+
+    // No es necesario volver a agregar <midi-visualizer> al contenedor
+  };
+
+  reader.readAsArrayBuffer(file);
 });
 
-// Event listener for MIDI file input
-midiFileInput.addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    if (file) {
-        playMIDI(file);
-    }
+const midiPlayer = document.querySelector('midi-player');
+const staffVisualizer = document.querySelector('#myVisualizer');
+
+// Detecta cuando la reproducción del MIDI comienza
+midiPlayer.addEventListener('click', () => {
+  
+  // Configura una animación para desplazar el contenido gradualmente
+  const scrollDuration = midiPlayer.duration / staffContent.scrollWidth;
+  setTimeout(()=>{
+    
+    staffVisualizer.scrollTo(staffVisualizer.scrollWidth, 0, scrollDuration);
+  }, 1000);
+  
 });
